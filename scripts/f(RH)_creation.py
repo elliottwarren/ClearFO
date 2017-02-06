dc75349e30fcf59b1d130434fdcd3c1d3bf0adfd
Elliott <e.l.warren@pgr.reading.ac.uk>
2017-02-06 17:15:04 +0000

    working on extracting aerosol data

--- scripts/f(RH)_creation.py
+++ scripts/f(RH)_creation.py
@@ -6,7 +6,7 @@ Created by Elliott 06/02/2017
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def main():
 
@@ -24,8 +24,11 @@ def main():
 
     ceil_lam = 0.91e-06
 
-    # variables to take from file (as listed within the file)
-    aerosols = ['Accum. Sulphate', 'Aitken Sulphate', 'Aged fossil-fuel OC']
+    # variables to take from file (as listed within the file) with index from BLOCK = 0
+    # NOTE: data MUST be in ascending index order
+    aerosols = {'Accum. Sulphate': 6, 'Aitken Sulphate': 7, 'Aged fossil-fuel OC': 22}
+    aer_order = ['Accum. Sulphate', 'Aitken Sulphate', 'Aged fossil-fuel OC']
+
 
     spec_bands = {'band': [],
                   'lower_limit': [],
@@ -64,12 +67,83 @@ def main():
     for key, value in spec_bands.iteritems():
         spec_bands[key] = np.array(value)
 
+    # find the band for current ceilometer wavelength
 
-    file = open(file_path, "r")
-    line = file.readline()
+    file.close()
 
 
 
+
+    data = {}
+
+
+    for aer in aer_order:
+
+        data[aer] = []
+
+        file = open(file_path, "r")
+
+        line = file.readline()  # read the first actual line
+        line = line.rstrip('\n\r')
+
+        while line != '*BLOCK: TYPE =   11: SUBTYPE =    1: VERSION =    2':
+            line = file.readline()
+            line = line.rstrip('\n\r')
+            line = ' '.join(line.split())
+
+        while line != 'Index of species = 6 Accum. Sulphate':
+            line = file.readline()
+            line = line.rstrip('\n\r')
+
+        while line != 'Band =    4':
+            line = file.readline()
+            line = line.rstrip('\n\r')
+
+        # skip two lines
+        line = file.readline()
+        line = file.readline()
+
+        # read first line of data
+        line = file.readline()
+        line = ' '.join(line.split()) # remove duplicate, trailing and leading spaces
+        line_split = line.split(' ')
+
+        # when reached the data line...
+        while line != 'Band =    5':
+
+            line = ' '.join(line.split()) # remove duplicate, trailing and leading spaces
+            line_split = line.split(' ')
+
+            # extract data
+            data[aer] += [line_split]
+
+            line = file.readline()
+            line = line.rstrip('\n\r')
+
+        file.close()
+
+        # convert to numpy array, list already alligned correctly for np.array()
+        data[aer] = np.array(data[aer])
+
+
+
+
+
+
+
+
+
+    # line = line.rstrip('\n\r')
+    #
+    # # skip until correct block is reached
+    # while line != '*BLOCK: TYPE =   11: SUBTYPE =    1: VERSION =    2':
+    #     pass
+    #
+    # for aer in aerosols.iterkeys():
+    #
+    #     while line != 'Index of species =     ' + block + '  Accum. Sulphate':
+    #         line = file.readline()
+    #         line = line.rstrip('\n\r')
 
 
 
