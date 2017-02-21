b0932e767b357a1dd6cf95bd35f5cd24616bed3f
Elliott <e.l.warren@pgr.reading.ac.uk>
2017-02-21 12:43:58 +0000

    took functions outside main():

--- scripts/f(RH)_creation.py
+++ scripts/f(RH)_creation.py
@@ -8,28 +8,14 @@ Created by Elliott 06/02/2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
+def read_spec_bands(file_path):
 
-    # setup
-    # find wavelength band
-    # read file
-    # find blocks
-    # format data
-    # create f(RH) curve using \sigma_ext(RH=0) from file as \sigma_ext(dry)
+    """
+    Reads in the spectral bands from UM spectral file
 
-    # directories
-    savedir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/clearFO/figures/f(RH)/'
-    datadir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/clearFO/data/Mie/'
-    file_name = 'spec3a_sw_hadgem1_7lean_so' # original file given to me by Claire Ryder 25/01/17
-    file_path = datadir + file_name
-
-    ceil_lam = 0.91e-06
-
-    # variables to take from file (as listed within the file) with index from BLOCK = 0
-    # NOTE: data MUST be in ascending index order
-    aer_index = {'Accum. Sulphate': 6, 'Aitken Sulphate': 7, 'Aged fossil-fuel OC': 22}
-    aer_order = ['Accum. Sulphate', 'Aitken Sulphate', 'Aged fossil-fuel OC']
-
+    :param file_path:
+    :return: spec_bands:
+    """
 
     spec_bands = {'band': [],
                   'lower_limit': [],
@@ -68,15 +54,24 @@ def main():
     for key, value in spec_bands.iteritems():
         spec_bands[key] = np.array(value)
 
-    # find the band for current ceilometer wavelength
-
     file.close()
 
+    return spec_bands
 
-    # read the humidity, abs and scat for each aerosol, in the current band.
+def read_aer_data(file_path, aer_index, aer_order):
+
+    """
+    Read in the aerosol data, calculate the Qs
+
+    :param file_path:
+    :param aer_index:
+    :param aer_order:
+    :param Q_type:
+    :return:
+    """
+
+    # define arrays
     data = {}
-    Q_ext = {}
-    f_RH = {}
 
     for aer in aer_order:
 
@@ -130,14 +125,90 @@ def main():
         # convert to numpy array, list already alligned correctly for np.array()
         data[aer] = np.array(data[aer], dtype=float)
 
-        # calculate total extinction (abs + scatt)
-        Q_ext[aer] = np.sum((data[aer][:, 1], data[aer][:, 2]), axis=0)
+
+    return data
+
+def calc_f_RH(data, aer_order, Q_type=''):
+
+    """
+    Calculate f(RH) for the Q type
+    :param data:
+    :param aer_order:
+    :param Q_type:
+    :return:
+    """
+
+    # define arrays
+    Q = {}
+    f_RH = {}
+
+    for aer in aer_order:
+
+        if Q_type == 'extinction':
+            # calculate total extinction (abs + scatt)
+            Q[aer] = np.sum((data[aer][:, 1], data[aer][:, 2]), axis=0)
+
+        elif Q_type == 'scattering':
+            # extract scattering
+            Q[aer] = data[aer][:, 2]
+
+        elif Q_type == 'absorption':
+            # extract absorption
+            Q[aer] = data[aer][:, 1]
+        else:
+            raise ValueError("Incorrect Q_type value. Needs to be either 'extinction', 'absorption', or 'scattering'")
 
         # calculate f(RH) = Q(RH>=0)/Q(RH=0)
-        f_RH[aer] = [Q_RH/Q_ext[aer][0] for Q_RH in Q_ext[aer]]
+        f_RH[aer] = [Q_RH / Q[aer][0] for Q_RH in Q[aer]]
 
-    # create an average one
+    return Q, f_RH
+
+def main():
+
+    # setup
+    # find wavelength band
+    # read file
+    # find blocks
+    # format data
+    # create f(RH) curve using \sigma_ext(RH=0) from file as \sigma_ext(dry)
+
+    # directories
+    savedir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/clearFO/figures/Mie/f(RH)/'
+    datadir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/clearFO/data/Mie/'
+    # file_name = 'spec3a_sw_hadgem1_7lean_so' # original file given to me by Claire Ryder 25/01/17
+    file_name = 'sp_sw_ga7'
+    file_path = datadir + file_name
+
+    ceil_lam = 0.91e-06
+
+    # variables to take from file (as listed within the file) with index from BLOCK = 0
+    # NOTE: data MUST be in ascending index order
+    if file_name == 'spec3a_sw_hadgem1_7lean_so':
+        aer_index = {'Accum. Sulphate': 6, 'Aitken Sulphate': 7, 'Aged fossil-fuel OC': 22}
+        aer_order = ['Accum. Sulphate', 'Aitken Sulphate', 'Aged fossil-fuel OC']
+    else:
+        aer_index = {'Accum. Sulphate': 14, 'Aitken Sulphate': 15, 'Aged fossil-fuel OC': 24, 'Ammonium nitrate': 26}
+        aer_order = ['Accum. Sulphate', 'Aitken Sulphate', 'Aged fossil-fuel OC', 'Ammonium nitrate']
+
+    # ---------------------------------------------------
+    # Read and Process
+    # ---------------------------------------------------
+
+    # read in the spectral band information
+    spec_bands = read_spec_bands(file_path)
+
+    # read the aerosol data
+    data = read_aer_data(file_path, aer_index, aer_order)
+
+    # calculate f(RH)
+    Q, f_RH = calc_f_RH(data, aer_order, Q_type='extinction')
+
+    # create an average f(RH)
     f_RH['average'] = np.mean(f_RH.values(), axis=0)
+
+    # ---------------------------------------------------
+    # Plotting
+    # ---------------------------------------------------
 
     # plot the data at the end so it is all neat and together
     fig = plt.figure(figsize=(6, 4))
@@ -146,15 +217,14 @@ def main():
         plt.plot(value[:, 0], f_RH[key], label = key, linestyle='--')
 
     # plot the average one (use RH from the last data.iteritems()
-    plt.plot(value[:, 0], f_RH['average'], label = 'average', color='black')
+    plt.plot(value[:, 0], f_RH['average'], label='average', color='black')
 
     plt.legend(fontsize=9, loc='best')
     plt.xlabel('RH')
     plt.ylabel('extinction f(RH)')
     plt.ylim([0, 8.0])
-    plt.title('690-1190nm band')
-    plt.savefig(savedir + 'ext_f_RH_690-1190nm.png')
-
+    plt.title(file_name + ': 690-1190nm band')
+    plt.savefig(savedir + file_name + '_ext_f_RH_690-1190nm.png')
 
 
 if __name__ == '__main__':
