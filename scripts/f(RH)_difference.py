4a68414d507ad25cc360d9dc8d6cde6753790ed0
Elliott <e.l.warren@pgr.reading.ac.uk>
2017-04-06 13:48:13 +0100

    initial commit

---
+++ scripts/f(RH)_difference.py
@@ -0,0 +1,9 @@
+"""
+Create 4 panel plot with f(RH) differences for each of the aerosol species and the average f(RH)
+
+Created by Elliott 06/04/17
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import f(RH)_creation
