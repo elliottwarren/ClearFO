5a0bfbbab200a321924ea736a830dfadaf857688
Elliott <e.l.warren@pgr.reading.ac.uk>
2017-04-27 14:28:33 +0100

    Initial commit

---
+++ scripts/longterm-OB-daily.py
@@ -0,0 +1,75 @@
+"""
+Hopefully the start of something good.
+Make daily plots with hourly statistics with standard deviations and everything
+
+Created by Elliott Thurs 27th April 2017
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
+from matplotlib.dates import DateFormatter
+
+import numpy as np
+import datetime as dt
+from scipy.stats import spearmanr
+
+import ellUtils as eu
+from forward_operator import FOUtils as FO
+from forward_operator import FOconstants as FOcon
+
+
+
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print 'END PROGRAM'
