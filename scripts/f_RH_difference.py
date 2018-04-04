0ac80886792d40344b920cde9e8ec592f983948b
elliott <e.l.warren@pgr.reading.ac.uk>
2018-04-04 16:18:50 +0100

    improved processing and plotting for paper 1

--- scripts/f_RH_difference.py
+++ scripts/f_RH_difference.py
@@ -6,11 +6,14 @@ Create 4 panel plot with f(RH) differences for each of the aerosol species and t
 Created by Elliott 06/04/17
 """
 
+import sys
+sys.path.append('C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/clearFO/scripts')
+
 import numpy as np
 import matplotlib.pyplot as plt
 from ellUtils import fig_majorAxis
 from f_RH_creation import read_aer_data, read_spec_bands, calc_f_RH
-
+from matplotlib.ticker import FormatStrFormatter
 
 def create_f_RH(file_paths, band, aer_index, aer_order, Q_type):
 
@@ -55,12 +58,17 @@ def create_f_RH(file_paths, band, aer_index, aer_order, Q_type):
         # define wavelength key using band_lam_range
         Q, f_RH[band_lam_range] = calc_f_RH(data, aer_order, Q_type=Q_type)
 
-        # create an average f(RH)
-        f_RH[band_lam_range]['average'] = np.mean(f_RH[band_lam_range].values(), axis=0)
+        # # create an average f(RH)
+        # f_RH[band_lam_range]['MURK'] = np.mean(f_RH[band_lam_range].values(), axis=0)
+
+        # create f(RH) for MURK
+        f_RH[band_lam_range]['MURK'] = np.sum([0.295 * np.array(f_RH[band_lam_range]['Accum. Sulphate']),
+                                              0.38 * np.array(f_RH[band_lam_range]['Aged fossil-fuel OC']),
+                                              0.325 * np.array(f_RH[band_lam_range]['Ammonium nitrate'])], axis=0)
+
         # f_RH['average'] = np.mean((f_RH['Accum. Sulphate'], f_RH['Aged fossil-fuel OC'], f_RH['Ammonium nitrate']), axis=0)
 
     return f_RH, band_lam_range, RH
-
 
 def calc_r_RH_difference(f_RH, f_RH_main, main_band_range):
 
@@ -85,7 +93,6 @@ def calc_r_RH_difference(f_RH, f_RH_main, main_band_range):
 
     return f_RH_diff
 
-
 def calc_r_RH_ratio(f_RH, f_RH_main, main_band_range):
     """
     Calculate the ratio of f(RH) between the various wavelengths (f_RH) and the reference (f_RH_main)
@@ -109,37 +116,39 @@ def calc_r_RH_ratio(f_RH, f_RH_main, main_band_range):
 
     return f_RH_ratio
 
-def plot_absolute(f_RH_main, f_RH, RH, savedir, aer_order, band_order, lam_colours, Q_type):
+# plotting
+
+def plot_absolute(f_RH_main, f_RH, RH, savedir, aer_order, aer_names, lam_colours, Q_type):
+
+    main_lam = f_RH_main.keys()[0]
 
     # plot the data on a 4 panel plot
     fig, ax = plt.subplots(2, 2, figsize=(8, 6))
 
-    # append the average to the aer_order to create a plotting order
-    aer_plot_order = aer_order + ['average']
+    # append MURK to the aer_order to create a plotting order and aer_names so it's name can be plotted
+    aer_plot_order = aer_order + ['MURK']
+    aer_names['MURK'] = 'MURK'
 
     # plot on each subplot fully, before doing the next one
     for ax_i, aerosol in zip(ax.flatten(), aer_plot_order):
 
-        # # plot all curves for a single aerosol, at a time
-        # for lam_range in f_RH.iterkeys():
-        #
-        #     ax_i.plot(RH, f_RH[lam_range][aerosol], color=lam_colours[lam_range], linestyle='--', label=lam_range)
-        #
-        # # plot 910 nm
-        # ax_i.plot(RH, f_RH_main['910-910nm'][aerosol], color='black', linestyle='--', label='910-910nm')
+        # plot all curves for a single aerosol, at a time
+        for lam_range in f_RH.iterkeys():
 
-        for lam_range, lam_i_colour in zip(band_order, lam_colours):
+            # ax_i.plot(RH, f_RH[lam_range][aerosol], color=lam_colours[lam_range], linestyle='--', label=lam_range)
+            ax_i.plot(RH, f_RH[lam_range][aerosol], linestyle='--')
 
-            ax_i.plot(RH, f_RH[lam_range][aerosol], color=lam_i_colour, linestyle='--', label=lam_range)
+        # plot 910 nm
+        # ax_i.plot(RH, f_RH_main[main_lam][aerosol], color='black', linestyle='--', label=main_lam)
 
-        ax_i.plot(RH, f_RH_main['910-910nm'][aerosol], color='black', linestyle='--', label='910-910nm')
 
 
         # prettify subplot
-        ax_i.set_title(aerosol)
+        ax_i.set_title(aer_names[aerosol])
         # ax_i.set_ylim([-0.2, 0.2])
         # ax_i.set_ylim([-1.0, 1.0])
         ax_i.set_xlim([0.0, 100.0])
+        ax_i.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 
     # prettify figure
     ax_main = fig_majorAxis(fig)
@@ -148,13 +157,12 @@ def plot_absolute(f_RH_main, f_RH, RH, savedir, aer_order, band_order, lam_colou
     plt.subplots_adjust(top=0.9, right=0.8)
     ax.flatten()[1].legend(fontsize=8, bbox_to_anchor=(1.07, 1), loc=2, borderaxespad=0.0)
     fig.suptitle('Absolute value (lam_i)')
-    plt.savefig(savedir + 'absolute/multi-panel-band_absolute_vs910'+ '_' + Q_type[0:3] + '_f_RH.png')
+    plt.savefig(savedir + 'absolute/multi-panel-ceils_absolute_905_' + Q_type[0:3] + '_f_RH.png')
     plt.close()
 
     return fig
 
-
-def plot_difference(f_RH_diff, RH, savedir, aer_order, band_order, lam_colours, Q_type):
+def plot_difference(f_RH_diff, RH, savedir, aer_order, aer_names, lam_colours, Q_type):
 
     """
     Plot the ratio of the f(RH) references compared to the main f(RH) curve
@@ -171,25 +179,26 @@ def plot_difference(f_RH_diff, RH, savedir, aer_order, band_order, lam_colours, 
     # plot the data on a 4 panel plot
     fig, ax = plt.subplots(2, 2, figsize=(8, 6))
 
-    # append the average to the aer_order to create a plotting order
-    aer_plot_order = aer_order + ['average']
+    # append MURK to the aer_order to create a plotting order and aer_names so it's name can be plotted
+    aer_plot_order = aer_order + ['MURK']
+    aer_names['MURK'] = 'MURK'
 
     # plot on each subplot fully, before doing the next one
     for ax_i, aerosol in zip(ax.flatten(), aer_plot_order):
 
-        # # plot all curves for a single aerosol, at a time
-        # for lam_range in f_RH_diff.iterkeys():
-        #
-        #     ax_i.plot(RH, f_RH_diff[lam_range][aerosol], color=lam_colours[lam_range], label=lam_range)
-
         # plot all curves for a single aerosol, at a time
-        for lam_range, lam_i_colour in zip(band_order, lam_colours):
+        for lam_range in f_RH_diff.iterkeys():
 
-            ax_i.plot(RH, f_RH_diff[lam_range][aerosol], color=lam_i_colour, label=lam_range)
+            ax_i.plot(RH, f_RH_diff[lam_range][aerosol])
+
+        # # plot all curves for a single aerosol, at a time
+        # for lam_range, lam_i_colour in zip(band_order, lam_colours):
+        #
+        #     ax_i.plot(RH, f_RH_diff[lam_range][aerosol], color=lam_i_colour, label=lam_range)
 
 
         # prettify subplot
-        ax_i.set_title(aerosol)
+        ax_i.set_title(aer_names[aerosol])
         # ax_i.legend()
         # ax_i.set_ylim([-0.2, 0.2])
         ax_i.set_xlim([0.0, 100.0])
@@ -203,13 +212,12 @@ def plot_difference(f_RH_diff, RH, savedir, aer_order, band_order, lam_colours, 
     # ax_main.set_ylabel('difference \n lam_i - 910nm')
 
     fig.suptitle('Absolute difference (lam_i - 910nm)')
-    plt.savefig(savedir + 'difference/multi-panel-band_difference_vs910'+ '_' + Q_type[0:3] + '_f_RH.png')
+    plt.savefig(savedir + 'difference/multi-panel-ceils_diff_905_' + Q_type[0:3] + '_f_RH.png')
     plt.close()
 
     return fig
 
-
-def plot_ratio(f_RH_ratio, RH, savedir, aer_order, band_order, lam_colours, Q_type):
+def plot_ratio(f_RH_ratio, RH, savedir, aer_order, aer_names, lam_colours, Q_type):
 
     """
     Plot the ratio of the f(RH) references compared to the main f(RH) curve
@@ -224,44 +232,44 @@ def plot_ratio(f_RH_ratio, RH, savedir, aer_order, band_order, lam_colours, Q_ty
     """
 
     # plot the data on a 4 panel plot
-    fig, ax = plt.subplots(2, 2, figsize=(8, 6))
+    fig, ax = plt.subplots(1, 1, figsize=(5.5, 3.5))
 
-    # append the average to the aer_order to create a plotting order
-    aer_plot_order = aer_order + ['average']
+    # append MURK to the aer_order to create a plotting order and aer_names so it's name can be plotted
+    aer_plot_order = aer_order + ['MURK']
+    aer_names['MURK'] = 'MURK'
 
     # plot on each subplot fully, before doing the next one
-    for ax_i, aerosol in zip(ax.flatten(), aer_plot_order):
+    for aerosol in aer_plot_order:
 
-        # # plot all curves for a single aerosol, at a time
-        # for lam_range in f_RH_ratio.iterkeys():
-        #
-        #     ax_i.plot(RH, f_RH_ratio[lam_range][aerosol], color=lam_colours[lam_range], label=lam_range)
-
+        # plot between ceilometers
         # plot all curves for a single aerosol, at a time
-        for lam_range, lam_i_colour in zip(band_order, lam_colours):
+        for lam_range in f_RH_ratio.iterkeys():
 
-            ax_i.plot(RH, f_RH_ratio[lam_range][aerosol], color=lam_i_colour, label=lam_range)
+            ax.plot(RH, f_RH_ratio[lam_range][aerosol], label=aerosol)
 
-        # prettify subplot
-        ax_i.set_title(aerosol)
-        #ax_i.set_ylim([1.05, 1.3])
-        ax_i.set_xlim([0.0, 100.0])
+    # prettify subplot
+    # ax.set_title(aer_names[aerosol])
+    ax.set_ylim([0.9, 1.3])
+    ax.set_xlim([0.0, 100.0])
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 
     # prettify figure
     ax_main = fig_majorAxis(fig)
     ax_main.set_xlabel('RH [%]')
-    ax_main.set_ylabel(r'$f_{ext},\/\lambda\/=\/i$' + '\n' + r'$f_{ext},\/\lambda\/=\/910nm$')
+    # ax_main.set_ylabel(r'$f_{ext},\/\lambda\/=\/i$' + '\n' + r'$f_{ext},\/\lambda\/=\/910nm$')
+    ax_main.set_ylabel(r'$\frac{f_{ext,rh}(\lambda=1064\/nm)}{f_{ext,rh}(\lambda=905\/nm)}$', labelpad=15, fontsize=15)
     plt.tight_layout()
-    plt.subplots_adjust(top=0.9, right=0.8)
-    ax.flatten()[1].legend(fontsize=8, bbox_to_anchor=(1.07, 1), loc=2, borderaxespad=0.0)
-    fig.suptitle('Ratio (lam_i / 910nm)')
-    plt.savefig(savedir + 'ratio/multi-panel-band_ratio_vs910'+ '_' + Q_type[0:3] + '_f_RH.png')
+    # plt.subplots_adjust(top=0.9, right=0.8)
+    ax.legend(fontsize=9)
+    # ax.flatten()[1].legend(fontsize=8, bbox_to_anchor=(1.07, 1), loc=2, borderaxespad=0.0)
+    # fig.suptitle('Ratio (lam_i / 910nm)')
+    plt.savefig(savedir + 'ratio/ceil_905vs1064_' + Q_type[0:3] + '_f_RH.png')
     plt.close()
 
     return fig
 
 
-def main():
+if __name__ == '__main__':
 
     # User set args
     # band that read_spec_bands() uses to find the correct band
@@ -279,26 +287,31 @@ def main():
     # file_name = 'sp_sw_ga7' # current UM file
     # file_name = 'sp_ew_910' # my own made file with 1 band at 910 nm
 
-    main_file = 'sp_ew_910'
+    main_file = 'sp_ew_ceil_905'
     main_path = specdir + main_file
+    main_file_band = 1 # 905 - 906
+
+    file_names = ['sp_ew_ceil_1063.95-1064.05']
 
     # file_names = ['sp_ew_ceil_guass_903-907',
-    #               'sp_ew_ceil_guass_908-912',
     #               'sp_ew_ceil_guass_1062-1066']
-
-    file_names = ['sp_908-912_r1.1e-7_stdev1.05_num1.0e8',
-                  'sp_908-912_r1.1e-7_stdev1.05_num8.0e9',
-                  'sp_908-912_r1.1e-7_stdev1.05_num2.0e10']
 
     file_paths = [specdir + f for f in file_names]
 
-    # variables to take from file (as listed within the file) with index from BLOCK = 0
-    # NOTE: data MUST be in ascending index order
-    aer_index = {'Accum. Sulphate': 1, 'Aged fossil-fuel OC': 2, 'Ammonium nitrate': 3}
-    aer_order = ['Accum. Sulphate', 'Aged fossil-fuel OC', 'Ammonium nitrate']
+    # index: variables to take from file (as listed within the file) with index from BLOCK = 0
+    #   NOTE: data MUST be in ascending index order
+    # order: plotting order
+    # names: titles to apply to each subplot
+    # aer_index = {'Accum. Sulphate': 1, 'Aged fossil-fuel OC': 2, 'Ammonium nitrate': 3}
+    # aer_order = ['Accum. Sulphate', 'Aged fossil-fuel OC', 'Ammonium nitrate']
+    # aer_names = {'Accum. Sulphate': 'Ammonium sulphate', 'Aged fossil-fuel OC': 'Organic carbon', 'Ammonium nitrate': 'Ammonium nitrate'}
+
+    aer_index = {'Ammonium Sulphate': 1, 'Aged fossil-fuel OC': 4, 'Ammonium nitrate': 5}
+    aer_order = ['Ammonium Sulphate', 'Aged fossil-fuel OC', 'Ammonium nitrate']
+    aer_names = {'Ammonium Sulphate': 'Ammonium sulphate', 'Aged fossil-fuel OC': 'Organic carbon', 'Ammonium nitrate': 'Ammonium nitrate'}
 
     # super rigid, needs replacing
-    lam_colours = {'1062-1066nm': 'r', '908-912nm': 'b', '903-907nm': 'g'}
+    lam_colours = {'1064nm': 'r', '905nm': 'g'}
 
 
     # Q type to use in calculating f(RH)
@@ -310,7 +323,7 @@ def main():
     # ---------------------------------------------------
 
     # create f(RH) for the main file
-    f_RH_main, main_band_range, RH = create_f_RH(main_path, band, aer_index, aer_order, Q_type)
+    f_RH_main, main_band_range, RH = create_f_RH(main_path, main_file_band, aer_index, aer_order, Q_type)
 
     # create f(RH) for all none-main files
     f_RH, _, _ = create_f_RH(file_paths, band, aer_index, aer_order, Q_type)
@@ -327,15 +340,16 @@ def main():
     # Plotting
     # ---------------------------------------------------
 
-    fig = plot_absolute(f_RH_main, f_RH, RH, savedir, aer_order, lam_colours, Q_type)
+    # make band_order = [], its a rough way to plot it but it works... this way the plotting funcitons still work
+    # with the multi-band versions
+
+    fig = plot_absolute(f_RH_main, f_RH, RH, savedir, aer_order, aer_names, lam_colours, Q_type)
 
     # plot the difference in f(RH): lam_i - lam_reference
-    fig = plot_difference(f_RH_diff, RH, savedir, aer_order, lam_colours, Q_type)
+    fig = plot_difference(f_RH_diff, RH, savedir, aer_order, aer_names,lam_colours, Q_type)
 
     # plot the ratio in f(RH): lam_i / lam_reference
-    fig = plot_ratio(f_RH_ratio, RH, savedir, aer_order, lam_colours, Q_type)
+    fig = plot_ratio(f_RH_ratio, RH, savedir, aer_order, aer_names, lam_colours, Q_type)
 
     print 'END PROGRAM'
 
-if __name__ == '__main__':
-    main()
